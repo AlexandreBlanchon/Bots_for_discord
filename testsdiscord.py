a2199936ef87bc7e9111c9433f34f652d403f225
AlexandreBlanchon <43498185+AlexandreBlanchon@users.noreply.github.com>
2018-09-22 21:43:18 +0200

    Add files via upload

---
+++ testsdiscord.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 19:07:12 2018
+
+@author: Alexandre
+"""
+
+
+import discord.ext.commands as disc
+import os
+
+
+TOKEN = os.environ['TOKEN']
+PREFIX = os.environ['PREFIX']
+client = disc.Bot(command_prefix = PREFIX)
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    
+@client.command()
+async def hello():
+    await client.say("Hello !")
+    
+client.run(TOKEN)
+
